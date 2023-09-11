cc1af8cd170daa25b0897ec9d18851927fea8044
user <legalas08@gmail.com>
2023-09-11 16:14:03 +0300

    [add] reset slider on a new top

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,6 +5,53 @@ let mqttClient;
 let url = document.querySelector('[data-url]').dataset.url
 let host = `ws://${url}/`;
 
+
+// this for reset slider
+const restartInterval = (func, delay) => {
+    let intervalId;
+
+    const startInterval = () => intervalId = setInterval(func, delay);
+    const stopInterval = () => clearInterval(intervalId);
+
+    const restart = () => {
+        stopInterval();
+        startInterval();
+    }
+
+    startInterval();
+
+    return restart;
+}
+
+// slider
+let sliderEngine = () => {
+    let robots = document.querySelectorAll('.robot')
+    robots.forEach(robot => {
+        // check if now robot not free
+        let robotStatus = robot.querySelector(".state").innerHTML
+        // let robotTime = robot.querySelector(".time").innerHTML
+
+        if(robotStatus !== 'Свободен'){
+            let hideElement = robot.querySelector('.hide');
+            let robotOption = hideElement.classList[0];
+
+            switch (robotOption) {
+                case 'state':
+                    robot.querySelector('.time').classList.add("hide")
+                    hideElement.classList.remove("hide")
+                    break;
+            
+                case 'time':
+                    robot.querySelector('.state').classList.add("hide")
+                    hideElement.classList.remove("hide")
+                    break;
+            }
+        }
+    });
+}
+
+const restart = restartInterval(sliderEngine, 3000); // Запустить интервал с функцией sliderEngine и задержкой 3000 миллисекунд
+
 let options = {
     clientId: 'mr_' + Math.random().toString(16).substring(2, 8),
     username: mqtt.username,
@@ -44,46 +91,38 @@ mqttClient.on("message", (topic, message) => {
     let robotId = topic[1];
     let robotOption = topic[2];
     let robotElement = document.querySelector(`[data-robot="${robotId}"]`);
- 
-
-    let setRobotState = (el, state) => {
-        el.innerHTML = state;
-    }
-    
-    let setRobotTime = (el, state) => {
-        el.innerHTML = state;
-    }
-
 
     // parse option robot
     switch (robotOption) {
         case "washing-time-left":
             if(message === '*'){
-                setRobotTime(robotElement.querySelector('.time'), 'Мойка окончена')
+                robotElement.querySelector('.time').innerHTML = 'Мойка окончена'
                 break;
             }
             if(message < 1){
-                setRobotTime(robotElement.querySelector('.time'), 'Менее минуты')
+                robotElement.querySelector('.time').innerHTML =  'Менее минуты'
                 
             } else {
-                setRobotTime(robotElement.querySelector('.time'), `${formatMinutes(message)}`)
+                robotElement.querySelector('.time').innerHTML = `${formatMinutes(message)}`
             }
             break;
         case 'state':
             switch (message) {
                 case '0':
-                    setRobotState(robotElement.querySelector('.state'), 'Свободен')
+                    robotElement.querySelector('.state').innerHTML = 'Свободен'
                     break;
                 case '1':
-                    setRobotState(robotElement.querySelector('.state'), 'Занят')
+                    robotElement.querySelector('.state').innerHTML =  'Занят'
                     break;
                 case '2':
-                    setRobotState(robotElement.querySelector('.state'), 'Недоступен')
+                    robotElement.querySelector('.state').innerHTML =  'Недоступен'
                     break;
             }
             break;
     }
 
+    // reset slider after new topic
+    restart()
 });
 
 // Subscribe on topics
@@ -92,30 +131,6 @@ mqttClient.subscribe('#', { qos: 0 });
 
 // DOM manipulation
 let listRobotsWrapper = document.querySelector(".wrapper");
-
-// slider status
-setInterval(() => {
-    let robots = document.querySelectorAll('.robot')
-    robots.forEach(robot => {
-        // check if now robot not free
-        if(robot.querySelector(".state").innerHTML !== 'Свободен'){
-            let hideElement = robot.querySelector('.hide');
-            let robotOption = hideElement.classList[0];
-
-            switch (robotOption) {
-                case 'state':
-                    robot.querySelector('.time').classList.add("hide")
-                    hideElement.classList.remove("hide")
-                    break;
-            
-                case 'time':
-                    robot.querySelector('.state').classList.add("hide")
-                    hideElement.classList.remove("hide")
-                    break;
-            }
-        }
-    });
-}, 3000)
 
 
 let formatMinutes = (minutes) => {
